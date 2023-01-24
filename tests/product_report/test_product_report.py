075454ab38442d5b6be298233739cc239e15418b
Eduardomak <eduardomak92@gmail.com>
2023-01-24 06:00:57 -0300

    test: Add product report test

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,24 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    # Seu teste deve ser escrito aqui
+    product = Product(
+        10,
+        "Titanium Dioxide",
+        "Target Corporation",
+        "2020-12-08",
+        "2023-12-08",
+        "FR295791533358XRG4PRIG28D08",
+        "instrucao 10",
+    )
+
+    product_string = product.__str__()
+
+    assert product_string == (
+        f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa} com validade"
+        f" até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
