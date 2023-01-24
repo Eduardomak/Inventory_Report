f80f5a565b7e0aa6db8cc71298bb2893b5462850
Eduardomak <eduardomak92@gmail.com>
2023-01-24 05:58:48 -0300

    feat: Add product test

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        10,
+        "Titanium Dioxide",
+        "Target Corporation",
+        "2020-12-08",
+        "2023-12-08",
+        "FR295791533358XRG4PRIG28D08",
+        "instrucao 10",
+    )
+    assert product.id == 10
+    assert product.nome_do_produto == "Titanium Dioxide"
+    assert product.nome_da_empresa == "Target Corporation"
+    assert product.data_de_fabricacao == "2020-12-08"
+    assert product.data_de_validade == "2023-12-08"
+    assert product.numero_de_serie == "FR295791533358XRG4PRIG28D08"
+    assert product.instrucoes_de_armazenamento == "instrucao 10"
